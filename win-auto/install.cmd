8d007aeb2c90c4bc423a6986759d860ec3d87ec1
salatielcf <77647084+salatielcf@users.noreply.github.com>
2025-08-12 22:32:07 -0300

    Update install.cmd

--- win-auto/install.cmd
+++ win-auto/install.cmd
@@ -25,7 +25,7 @@ SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 :: INSTALAÇÃO DOS APLICATIVOS
 :: ================================
 echo Instalando aplicativos via Chocolatey...
-choco install googlechrome jre8 firefox pdfcreator anydesk nanazip --ignore-checksums --no-progress -y
+choco install googlechrome jre8 firefox pdfcreator anydesk nanazip --ignore-checksums -y
 if errorlevel 1 (
     echo [ERRO] Falha na instalação de pacotes Chocolatey. >> "%LOG_FILE%"
     exit /b 2
@@ -47,8 +47,10 @@ echo Baixando instalador do Office...
 set ODT_SETUP_URL=https://github.com/scfti/scripts/raw/refs/heads/main/win-auto/office/setup.exe
 set ODT_XML_URL=https://github.com/scfti/scripts/raw/refs/heads/main/win-auto/office/configuration.xml
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri '%ODT_SETUP_URL%' -OutFile '%ODT_DIR%\Setup.exe' -UseBasicParsing -Headers @{ 'User-Agent' = 'Mozilla/5.0' }"
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri '%ODT_XML_URL%' -OutFile '%ODT_DIR%\configuration.xml' -UseBasicParsing -Headers @{ 'User-Agent' = 'Mozilla/5.0' }"
+:: powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri '%ODT_SETUP_URL%' -OutFile '%ODT_DIR%\Setup.exe' -UseBasicParsing -Headers @{ 'User-Agent' = 'Mozilla/5.0' }"
+:: powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri '%ODT_XML_URL%' -OutFile '%ODT_DIR%\configuration.xml' -UseBasicParsing -Headers @{ 'User-Agent' = 'Mozilla/5.0' }"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest '%ODT_SETUP_URL%' -OutFile '%ODT_DIR%\Setup.exe'; Invoke-WebRequest '%ODT_XML_URL%' -OutFile '%ODT_DIR%\configuration.xml'"
+
 
 if not exist "%ODT_DIR%\Setup.exe" (
     echo [ERRO] O arquivo Setup.exe nao foi baixado. >> "%LOG_FILE%"
@@ -60,7 +62,7 @@ if not exist "%ODT_DIR%\configuration.xml" (
 )
 
 :: Instala o Office
-start /wait "" "%ODT_DIR%\Setup.exe" /configure "%ODT_DIR%\configuration.xml"
+start /wait "%ODT_DIR%\Setup.exe" /configure "%ODT_DIR%\configuration.xml"
 if errorlevel 1 (
     echo [ERRO] Falha na instalacao do Office. >> "%LOG_FILE%"
     exit /b 5
@@ -70,7 +72,8 @@ if errorlevel 1 (
 :: ATIVADOR (USO CRÍTICO)
 :: ================================
 echo Executando ativador...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://get.activated.win | iex"
+:: powershell -NoProfile -ExecutionPolicy Bypass -Command "irm https://get.activated.win | iex"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& ([ScriptBlock]::Create((New-Object Net.WebClient).DownloadString('https://get.activated.win'))) /HWID /Ohook"
 if errorlevel 1 (
     echo [ALERTA] Ativador pode ter falhado. >> "%LOG_FILE%"
 )
